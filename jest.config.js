84f645fc8a933f6e1cd523d8a7dc5a24e91d9d82
Lucas Melo <lucasmelo@protonmail.com>
2021-02-01 16:12:18 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+export default {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**'
+  ],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
